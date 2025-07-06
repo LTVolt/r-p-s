670ad01b6f883b7741ea1045e60c474cd230244e
LTVolt <johnssanots7@gmail.com>
2025-07-06 14:16:41 +0100

    First iteration of this little project

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # r-p-s
 Rock Paper Scissors little project.
+
+Just open up the index.html file and play around in the console.
---
+++ java.js
@@ -0,0 +1,89 @@
+console.log("Game Start!")
+
+let computerSelection = computerChoice()
+let playerSelection = playerChoice()
+
+function computerChoice() {
+    let number = Math.random()
+    if (number < 0.33) {return "Rock"}
+    else if (number < 0.67) {return "Scissors"}
+    else return "Paper"
+}
+
+function playerChoice() {
+   return prompt("Please type Rock, Paper or Scissors")
+}
+
+let playerScore = 0
+let computerScore = 0
+
+function playRound (computer, player) {
+
+    let computerArranjed = String(computer).toLowerCase()
+    let playerArranjed = String(player).toLowerCase()
+
+    console.log(computerArranjed + " / " + playerArranjed)
+    if (computerArranjed === playerArranjed) {
+        console.log("Computer: " + computerScore + ", Player: " + playerScore)
+        console.log("It's a tie!")
+        return 
+    } else if (computerArranjed === "rock" && playerArranjed === "scissors") {
+        computerScore++
+        console.log("Computer: " + computerScore + ", Player: " + playerScore)
+        console.log("You lose! Rock beats Scissors")
+        return 
+    } else if (computerArranjed === "rock" && playerArranjed === "paper") {
+        playerScore++
+        console.log("Computer: " + computerScore + ", Player: " + playerScore)
+        console.log("You win! Paper beats Rock")
+        return 
+    } else if (computerArranjed === "scissors" && playerArranjed === "rock") {
+        playerScore++
+        console.log("Computer: " + computerScore + ", Player: " + playerScore)
+        console.log("You win! Rock beats Scissors")
+        return 
+    } else if (computerArranjed === "scissors" && playerArranjed === "paper") {
+        computerScore++
+        console.log("Computer: " + computerScore + ", Player: " + playerScore)
+        console.log("You lose! Scissors beats Paper")
+        return 
+    } else if (computerArranjed === "paper" && playerArranjed === "rock") {
+        computerScore++
+        console.log("Computer: " + computerScore + ", Player: " + playerScore)
+        console.log("You lose! Paper beats Rock")
+        return 
+    } else if (computerArranjed === "paper" && playerArranjed === "scissors") {
+        playerScore++
+        console.log("Computer: " + computerScore + ", Player: " + playerScore)
+        console.log("You win! Scissors beats Paper")
+        return 
+    } 
+}
+
+
+function playGame() {
+    console.log("Round 1!")
+    playRound (computerSelection, playerSelection)
+    computerSelection = computerChoice()
+    playerSelection = playerChoice()
+
+    console.log("Round 2!")
+    playRound (computerSelection, playerSelection)
+    computerSelection = computerChoice()
+    playerSelection = playerChoice()
+
+    console.log("Round 3!")
+    playRound (computerSelection, playerSelection)
+    computerSelection = computerChoice()
+    playerSelection = playerChoice()
+
+    console.log("Round 4!")
+    playRound (computerSelection, playerSelection)
+    computerSelection = computerChoice()
+    playerSelection = playerChoice()
+    
+    console.log("Round 5!")
+    playRound (computerSelection, playerSelection)
+}
+
+playGame()
